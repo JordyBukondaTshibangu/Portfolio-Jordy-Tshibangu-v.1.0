8622787c2238c29ec6042f6014cb5edd8896f53c
jordy_tshibss <jordytshibss@gmail.com>
2023-01-30 17:42:44 +0200

    features : slider library

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,21 @@
 const navMenu = document.getElementById("nav-menu");
 navToggle = document.getElementById("nav-toggle");
 navClose = document.getElementById("nav-close");
-
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
-
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
-
 const navLink = document.querySelectorAll(".nav__link");
-
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show-menu");
 }
-
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 const skillsContent = document.getElementsByClassName("skills__content");
@@ -35,14 +30,11 @@ function toggleSkills() {
     this.parentNode.className = "skills__content skills__open";
   }
 }
-
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
-
 const tabs = document.querySelectorAll("[data-target");
 tabContents = document.querySelectorAll("[data-content");
-
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target);
@@ -57,7 +49,6 @@ tabs.forEach((tab) => {
   });
   tab.classList.add("qualification__active");
 });
-
 const modalViews = document.querySelectorAll(".services__modal");
 modalBtns = document.querySelectorAll(".services__button");
 modalCloses = document.querySelectorAll(".services__modal-close");
@@ -104,14 +95,12 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
-
 function scrollHeader() {
   const nav = document.getElementById("header");
   if (this.scrollY >= 200) nav.classList.add("scroll-header");
   else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
-
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
@@ -119,7 +108,6 @@ function scrollUp() {
   else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
-
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "uil-sun";
@@ -144,7 +132,6 @@ if (selectedTheme) {
     iconTheme
   );
 }
-
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
   // Add or remove the dark / icon theme
@@ -153,4 +140,26 @@ themeButton.addEventListener("click", () => {
   // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
+});
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
